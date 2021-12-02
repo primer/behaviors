695858c6fe0707a37ab5e0cb631799ea26e0e92c
Dusty Greif <dgreif@users.noreply.github.com>
2021-12-02 15:44:48 -0700

    Port behaviors from Primer React (#1)
    Co-authored-by: Cole Bemis <colebemis@github.com>

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+// eslint-disable-next-line filenames/match-regex
+export default {
+  clearMocks: true,
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'esbuild-jest'
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export function add(a: number, b: number) {
-  return a + b
-}
+export * from './anchored-position.js'
+export * from './focus-trap.js'
+export * from './focus-zone.js'
+export * from './scroll-into-viewing-area.js'

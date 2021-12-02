a7d6f01953f6451f784de4085cc4062eeb97846a
Dusty Greif <dustin.greif@gmail.com>
2021-12-02 20:17:29 +0000

    Port behaviors from primer react

--- jest.config.js
+++ jest.config.js
@@ -5,16 +5,12 @@
 
 // eslint-disable-next-line filenames/match-regex
 export default {
-    clearMocks: true,
-    preset: 'ts-jest/presets/default-esm',
-    testEnvironment: 'jsdom',
-    globals: {
-      'ts-jest': {
-        useESM: true
-      }
-    },
-    moduleNameMapper: {
-      '^(\\.{1,2}/.*)\\.js$': '$1'
-    }
+  clearMocks: true,
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'esbuild-jest'
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
   }
-  
+}

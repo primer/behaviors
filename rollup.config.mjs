3b5f1eb894e0eb19072a5a92aaf04110f0356d03
Josh Black <joshblack@github.com>
2024-06-14 13:26:12 -0500

    refactor: emit package.json with "type" instead of .mjs files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -14,20 +14,27 @@ const external = Array.from(dependencies).map(name => {
   return new RegExp(`^${name}(/.*)?`)
 })
 
-function createPackageRegex(name) {
-  return new RegExp(`^${name}(/.*)?`)
-}
-
 export default [
   // ESM
   {
     input: ['src/index.ts', 'src/utils/index.ts'],
     external,
-    plugins: [typescript()],
+    plugins: [
+      typescript(),
+      {
+        id: 'emit-package-json-file',
+        generateBundle() {
+          this.emitFile({
+            type: 'asset',
+            source: JSON.stringify({type: 'module'}),
+            fileName: 'package.json',
+          })
+        },
+      },
+    ],
     output: {
       dir: 'dist/esm',
       format: 'esm',
-      entryFileNames: '[name].mjs',
       preserveModules: true,
       preserveModulesRoot: 'src',
     },

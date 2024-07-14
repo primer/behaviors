67215a51ce82a967ced2264ba19a28e0d0d43592
Josh Black <joshblack@github.com>
2024-07-14 16:01:52 -0500

    feat(npm): add support for resolving "import" conditions to ESM (#400)
    * feat(project): emit .mjs files for ESM bundle
    
    * chore: add changeset
    
    * fix: update references in exports to use .mjs
    
    * fix: update entry for size-limit
    
    * ci: update type-check job
    
    * refactor: emit package.json with "type" instead of .mjs files
    
    * chore: update size-limit entrypoint
    
    * chore: update changeset
    
    * chore: use explicit .mjs extension
    
    * chore: add optional dependencies for ci
    
    * chore: update size-limit entry point

---
+++ .changeset/clean-kangaroos-repeat.md
@@ -0,0 +1,5 @@
+---
+'@primer/behaviors': minor
+---
+
+Update the output of the package to correctly map "import" conditions to ESM
---
+++ rollup.config.mjs
@@ -0,0 +1,60 @@
+import typescript from '@rollup/plugin-typescript'
+import packageJson from './package.json' assert {type: 'json'}
+
+const dependencyTypes = ['peerDependencies', 'dependencies', 'devDependencies']
+const dependencies = new Set(
+  dependencyTypes.flatMap(type => {
+    if (packageJson[type]) {
+      return Object.keys(packageJson[type])
+    }
+    return []
+  }),
+)
+const external = Array.from(dependencies).map(name => {
+  return new RegExp(`^${name}(/.*)?`)
+})
+
+export default [
+  // ESM
+  {
+    input: ['src/index.ts', 'src/utils/index.ts'],
+    external,
+    plugins: [
+      typescript(),
+      {
+        id: 'emit-package-json-file',
+        generateBundle() {
+          this.emitFile({
+            type: 'asset',
+            source: JSON.stringify({type: 'module'}),
+            fileName: 'package.json',
+          })
+        },
+      },
+    ],
+    output: {
+      dir: 'dist/esm',
+      format: 'esm',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      entryFileNames: '[name].mjs',
+    },
+  },
+
+  // CommonJs
+  {
+    input: ['src/index.ts', 'src/utils/index.ts'],
+    external,
+    plugins: [
+      typescript({
+        outDir: 'dist/cjs',
+      }),
+    ],
+    output: {
+      dir: 'dist/cjs',
+      format: 'commonjs',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+    },
+  },
+]

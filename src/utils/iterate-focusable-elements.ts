4d6dd33aaaf8f090159054752979b22a3a64261b
Jon Rohan <yes@jonrohan.codes>
2024-07-26 22:30:17 +0000

    Fix lint error

--- src/utils/iterate-focusable-elements.ts
+++ src/utils/iterate-focusable-elements.ts
@@ -31,7 +31,7 @@ export function* iterateFocusableElements(
   options: IterateFocusableElements = {},
 ): Generator<HTMLElement, undefined, undefined> {
   const strict = options.strict ?? false
-  const acceptFn = options.onlyTabbable ?? false ? isTabbable : isFocusable
+  const acceptFn = (options.onlyTabbable ?? false) ? isTabbable : isFocusable
   const walker = document.createTreeWalker(container, NodeFilter.SHOW_ELEMENT, {
     acceptNode: node =>
       node instanceof HTMLElement && acceptFn(node, strict) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP,
